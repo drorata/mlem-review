b25d8c3a8a9b3dd3606131665e11741014022916
Dror Atariah <drorata@gmail.com>
2022-05-30 16:31:05 +0200

    Improved data fetching; train/test splitting

--- get_data.py
+++ get_data.py
@@ -1,7 +1,15 @@
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
 
 
 X, y = load_iris(return_X_y=True, as_frame=True)
 
-X.to_pickle("./data/raw_X.pkl")
-y.to_pickle("./data/raw_y.pkl")
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, shuffle=True, random_state=42
+)
+
+X_train.to_pickle("./data/raw_X.pkl")
+y_train.to_pickle("./data/raw_y.pkl")
+
+X_test.to_pickle("./data/X_test.pkl")
+y_test.to_pickle("./data/y_test.pkl")
